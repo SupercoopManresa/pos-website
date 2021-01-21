d0f53cf166bcd1be9062bcd53373df353bd60f77
Supercoop Manresa <informatica@supercoopmanresa.cat>
2021-01-21 12:50:08 +0100

    Add files via upload

--- static/src/js/custom.js
+++ static/src/js/custom.js
@@ -1,1 +1,12 @@
-khgv
+odoo.define('affiliate_testinomial_extension.custom',function(require){
+"use strict";
+
+var core = require('web.core');
+var ajax = require('web.ajax');
+
+var _t = core._t;
+
+
+
+
+});
